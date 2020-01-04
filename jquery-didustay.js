b0f57618ae62b6c68e406cb8e02b3a1f4348acb2
Daniel Ossipoff <ossipoff@gmail.com>
2020-01-04 23:24:13 +0100

    complete rewrite of didustay

--- jquery-didustay.js
+++ jquery-didustay.js
@@ -1,61 +1,40 @@
 (function(window, $) {
-  var window = this,
-  ua = window.navigator.userAgent,
-  windowsMatch = /windows nt (\d+.\d+)/i.exec(ua),
-  windowsVersion = !!windowsMatch ? windowsMatch[1] : null,
-  isFireFox = /.*firefox\/\d/i.test(ua),
-  isIE = /.*msie|trident\/\d/i.test(ua),
-  isEdge = /.*edge\/\d/i.test(ua),
-  isOpera = /.*opr\/\d/i.test(ua),
-  isChrome = !isEdge && !isOpera && /.*chrome\/\d/i.test(ua),
-  isIEMetro = isIE && windowsVersion > 6.1 && windowsVersion < 6.4 && window.screenY === 0 && (window.innerHeight+1) !== window.outerHeight,
-  isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(ua);
-  
-  if (isMobile) {
-    console.warn('didustay doesn\'t work on mobile platforms!');
-  } else if (!windowsVersion) {
-    console.warn('didustay only works on Windows platform!');
-  } else {
-    $.fn.didustay = function(f) {
-      this.each(function() {
-        if (this === window) {
-          $(this).on('beforeunload', function(e) {
-            setTimeout(function() {
-              $(window.document).one('mousemove', function(e) {
-                var xOffset, yOffset;
-                
-                if (isEdge) {
-                    xOffset = 0;
-                  } else if (isOpera) {
-                    xOffset = 155;
-                  } else if (isChrome) {
-                    xOffset = 120;
-                  } else if (isFireFox) {
-                    xOffset = 200;
-                  } else if (isIEMetro) {
-                    xOffset = 245;
-                  } else if (isIE) {
-                    yOffset = -35;
-                }
-                
-                if (xOffset !== undefined) {
-                  if (e.pageX > ($(window).width() / 2) + xOffset) {
-                    f(e);
-                  }
-                } else if (yOffset !== undefined) {
-                  if (e.pageY > ($(window).height() / 2) + yOffset) {
-                    f(e);
-                  }
-                } else {
-                  f(e);
-                }
-              });
-            }, 1);
+  $.fn.didustay = function(options) {
+    options = $.extend({
+      stayed: function() {},
+      left: function() {}
+    }, options);
+
+    this.each(function() {
+      if (this === window) {
+        var isBeforeunload = false;
+        var isUnload = false;
+        var $iframe = $('<iframe style="width:0;height:0;border:none;position:absolute;top:-1px;left:-1px;" />');
+
+        $iframe.on('load', function() {
+          $($iframe[0].contentWindow).on('beforeunload', function() {
+            if (isBeforeunload) {
+              isUnload = true;
+            }
           });
-        } else {
-          throw new Error('didustay only works for window object!');
-        }
-      });
-    };
-  }
+        });
+
+        $(this.document).find('body').append($iframe);
+
+        $(this).on('beforeunload', function(e) {
+          isBeforeunload = true;
+          $(this).one('focus', function (e) {
+            if (!isUnload) {
+              options.stayed();
+            } else {
+              options.left();
+            }
+            isBeforeunload = false;
+          });
+        });
+      } else {
+        throw new Error('didustay only works for window object!');
+      }
+    });
+  };
 })(window, jQuery);
